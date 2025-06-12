be2b4a93db937d3ed75411f4ca8bf5c873dcfa73
= <=>
2025-06-12 22:31:31 +0800

    Add new syntax updates, exclude android folder

--- scene/login.gd
+++ scene/login.gd
@@ -15,5 +15,28 @@ func _on_login_pressed():
 		print("Password is required.")
 		return
 	else:
-		var dashboard = load("res://scene/dashboard.tscn")
-		get_tree().change_scene_to_packed(dashboard)
+		var data = load_data()
+		if data:
+			if username.text == data.get("username", "") and password.text == data.get("password", ""):
+				print("Login successful.")
+				var dashboard = load("res://scene/dashboard.tscn")
+				get_tree().change_scene_to_packed(dashboard)
+			else:
+				print("Invalid username or password.")
+		else:
+			print("No user data found.")
+
+func load_data():
+	if FileAccess.file_exists("user://user_data.json"):
+		var file = FileAccess.open("user://user_data.json", FileAccess.READ)
+		var content = file.get_as_text()
+		file.close()
+
+		var result = JSON.parse_string(content)
+		if typeof(result) == TYPE_DICTIONARY:
+			return result
+		else:
+			print("Failed to parse JSON.")
+	else:
+		print("User data file not found.")
+	return null
--- scene/register.gd
+++ scene/register.gd
@@ -1,10 +1,42 @@
 extends Control
 
 @onready var loginbtn = $"BoxContainer/Button2"
+@onready var regbtn = $"BoxContainer/Button"
+@onready var username = $"BoxContainer/username-input"
+@onready var email = $"BoxContainer/email-input"
+@onready var month = $"BoxContainer/HBoxContainer/month-input"
+@onready var day = $"BoxContainer/HBoxContainer/day-input"
+@onready var year = $"BoxContainer/HBoxContainer/year-input"
+@onready var password = $"BoxContainer/password-input"
+@onready var confirmpass = $"BoxContainer/confirmpass-input"
 
 func _ready():
 	loginbtn.pressed.connect(_on_login_pressed)
+	regbtn.pressed.connect(_on_register_pressed)
 
+func _on_register_pressed():
+	
+	var loginscreen = load("res://scene/login.tscn")
+	
+	if password.text == confirmpass.text:
+		var data = {
+			"username":username.text,
+			"email":email.text,
+			"birthday":month.text + "" + day.text + "" + year.text,
+			"password": password.text,
+		}
+		
+		var json_string = JSON.stringify(data)
+		
+		var file = FileAccess.open("user://user_data.json", FileAccess.WRITE)
+		file.store_string(json_string)
+		file.close()
+		get_tree().change_scene_to_packed(loginscreen)
+		print("Data saved.")
+	else:
+		print("Password doesn't match!")
+	
+		
 func _on_login_pressed():
 	var loginscreen = load("res://scene/login.tscn")
 	get_tree().change_scene_to_packed(loginscreen)
